d84cc483d933aa22a63e76dbc6ec014c28b86b93
OskarsOss <oss.oskars@gmail.com>
2023-09-22 17:48:23 +0300

    initial controller and service for calculate api

--- src/main/java/com/hw/bicyclepolicyrestservice/controller/BicyclePolicyController.java
+++ src/main/java/com/hw/bicyclepolicyrestservice/controller/BicyclePolicyController.java
@@ -1,9 +1,18 @@
 package com.hw.bicyclepolicyrestservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hw.bicyclepolicyrestservice.model.PremiumRequest;
+import com.hw.bicyclepolicyrestservice.model.PremiumResponse;
 import com.hw.bicyclepolicyrestservice.service.BicyclePolicyService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class BicyclePolicyController {
@@ -14,6 +23,11 @@ public class BicyclePolicyController {
 	public BicyclePolicyController(BicyclePolicyService service) {
 		this.service = service;
 	}
-
+	
+	@RequestMapping(value = "/calculate", method = RequestMethod.POST)
+	public ResponseEntity<Object> calculate(@Valid @RequestBody PremiumRequest request) {
+		PremiumResponse response = service.calculatePremium(request.getBicycles());
+		return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+	}
 
 }
--- src/main/java/com/hw/bicyclepolicyrestservice/service/BicyclePolicyService.java
+++ src/main/java/com/hw/bicyclepolicyrestservice/service/BicyclePolicyService.java
@@ -1,8 +1,65 @@
 package com.hw.bicyclepolicyrestservice.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.hw.bicyclepolicyrestservice.model.Bicycle;
+import com.hw.bicyclepolicyrestservice.model.InsuredObject;
+import com.hw.bicyclepolicyrestservice.model.PremiumResponse;
+import com.hw.bicyclepolicyrestservice.model.Risk;
 
 @Service
 public class BicyclePolicyService {
+	
+	public PremiumResponse calculatePremium(List<Bicycle> bicycles) {
+		
+		PremiumResponse result = new PremiumResponse();
+		
+		// TODO
+		double totalPremium = 99.19;
+		List<InsuredObject> insuredObjects = new ArrayList<InsuredObject>();
+		
+		for (Bicycle bicycle : bicycles) {			
+			InsuredObject insuredObject = new InsuredObject();
+			insuredObject.setCoverageType(bicycle.getCoverage());
+			insuredObject.setSumInsured(bicycle.getSumInsured());
+			insuredObject.addAttribute("MANUFACTURE_YEAR", bicycle.getManufactureYear());
+			insuredObject.addAttribute("MODEL", bicycle.getModel());
+			insuredObject.addAttribute("MAKE", bicycle.getMake());
+			
+			List<Risk> risks = new ArrayList<Risk>();
+			double calculatedPremium = 0;
+			for (String riskName : bicycle.getRisks()) {
+				Risk risk = new Risk(riskName);
+				
+				double riskPremium = 0;
+				double riskSumInsured = 0;
+				// TODO call groovy calculations
+				
+				risk.setSumInsured(riskSumInsured);
+				risk.setPremium(riskPremium);
+				risks.add(risk);
+				
+				calculatedPremium += riskPremium;
+			}
+			
+			
+			insuredObject.setPremium(calculatedPremium);
+			insuredObject.setRisks(risks);
+			
+			
+			
+			insuredObjects.add(insuredObject);
+			
+			totalPremium += calculatedPremium;
+		}
+		
+		result.setObjects(insuredObjects);
+		result.setPremium(totalPremium);
+		
+		return result;
+	}
 
 }
